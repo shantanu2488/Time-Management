f2339cbdabe8387aa38cdfd2a6431e0991e7734a
shantanu Bamnavat <shantanu@pocketnode.net>
2020-04-01 16:58:44 +0530

    hideNavbar

--- src/Components/Routes.js
+++ src/Components/Routes.js
@@ -8,13 +8,14 @@ import Signup from './Signup';
 export default class Routes extends Component  {
     render() {
         return (
-            <Router>
-                <Stack key="root">
-                    <Scene key="login" component={Login} title="Login" initial={true}/>
-                    <Scene key="signup" component={Signup} title="Register"/>
-                </Stack>
-            </Router>
-        
+           <Router>
+               <Stack key='root' hideNavBar={true}>
+                   <Scene key='login' component={Login} title='Login' initial={true} />
+                   <Scene key='signup' component={Signup} title='Register' />
+
+               </Stack>
+
+           </Router>
 
         )
 
