a0c5e3cf615d568b99b885d611951cf7e562ba9f
shantanu Bamnavat <shantanu@pocketnode.net>
2020-04-04 15:22:18 +0530

    add store to app

--- App.js
+++ App.js
@@ -11,32 +11,22 @@ import {
   Image,
 } from 'react-native';
 import {Provider} from 'react-redux';
+import persist from './src/config/store';
+import Main from './src/Main';
+import {PersistGate} from 'redux-persist/integration/react';
 
-import Routes from './src/Components/Routes';
-import store from './src/config/store';
-
+const persistStore = persist();
 
 export default class App extends Component {
   render() { 
     return (
-      <Provider store={store}>
-      <Routes />
+      <Provider store={persistStore.store}>
+        <PersistGate loading={null} persistor={persistStore.persistor}>
+      <Main />
+      </PersistGate>
       </Provider>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#777',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  text: {
-    color: '#fff',
-    fontSize: 30,
 
-
-  },
-});
